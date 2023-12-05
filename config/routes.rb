832590e2e02827af9557842a7d9733bde40a34ca
phillipcrawford <phillip.crawford3@gmail.com>
2023-12-05 16:33:47 -0600

    added token in comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # 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
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
