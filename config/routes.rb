1a42707450abfa2ebb65d794891a491e247135b4
phillipcrawford <phillip.crawford3@gmail.com>
2023-12-09 06:31:32 -0600

    define a show route in controller and make a show view for an individual article

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
   # The route above declares that GET /articles requests are mapped to the index action of ArticlesController
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/articles/:id", to: "articles#show"
+  
   get "/foo", to: "foo#fundex"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index" 
